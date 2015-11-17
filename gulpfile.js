6364e32734cace11b032fad6ede36c2f8b4593fb
harry <langzhi@jiankongbao.com>
2015-11-17 18:59:12 +0800

    add gulp-requirejs

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp'),
-    rjs = require('gulp-requirejs'),
-    concat = require('gulp-concat');
+    rjs = require('gulp-requirejs');
 
 gulp.task('mainjs', function () {
     return gulp.src('/')
@@ -32,19 +31,7 @@ gulp.task('mainjs', function () {
         .pipe(gulp.dest('app/dest')); // pipe it to the output DIR
 });
 
-gulp.task('title1', function () {
-    return gulp.src(['app/views/title1/controller.js', 'app/views/title1/index.js'])
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('app/views/title1/dist')); // pipe it to the output DIR
-});
-
-gulp.task('title2', function () {
-    return gulp.src(['app/views/title2/controller.js', 'app/views/title2/service.js', 'app/views/title2/index.js'])
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('app/views/title2/dist')); // pipe it to the output DIR
-});
-
-gulp.task('default', ['mainjs', 'title1', 'title2'], function () {
+gulp.task('default', ['mainjs'], function () {
 
     console.log('gulp tasks end!!');
 
